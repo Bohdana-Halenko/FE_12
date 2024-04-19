adb652d31b6cab0eb3ad5617e826d59840ff4628
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-04-19 23:21:33 +0300

    19.04

---
+++ JS1/script.js
@@ -0,0 +1,40 @@
+"use strict"
+
+// const b = 10;
+// a = b * 2;
+
+// console.log("hello goiteens".toLocaleUpperCase());
+
+// var b = 10;
+// var age = 15;
+
+// const b = 10;
+
+// let b = 10;
+
+// b = 10;
+
+// let age = 18;
+// age = 19;
+// console.log(age);
+
+// const userName = "Анна";
+// console.log(userName);
+
+// const MAIN_COLOR = ""
+
+// const a = 10.5;
+// const name = "Anna";
+// const age = true;
+
+
+// let userName = false;
+// console.log(typeof userName);
+
+// const message = "Привіт";
+// // console.log(message);
+// alert(message);
+
+
+const message = prompt("Чи погоджуєтесь ви з умовами?");
+console.log(message);
