bfb838d8665fcbb99aba5b356400b39b573a01c0
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-04-24 23:54:56 +0300

    Update script.js

---
+++ JS2/script.js
@@ -0,0 +1,25 @@
+const x = 5;
+const y = 2;
+const z = 5;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+
+console.log(x > y);
+console.log(x < y);
+console.log(x <= z);
+console.log(x == y);
+console.log(x == z);
+console.log(x != z);
+console.log(x === y);
+console.log(x !== y);
+
+const a = '5';
+console.log(Number(a));
+console.log(typeof Number(a));
+
+const b = 'hello';
+console.log(Number(b));
