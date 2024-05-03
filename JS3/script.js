6db2d2e20ff497d5747658625e9e561604577fd9
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-05-03 22:04:23 +0300

    03.05

--- JS3/script.js
+++ JS3/script.js
@@ -19,7 +19,7 @@ b = '5' + 5 + 5;
 const c = 'Welcome to Bahamas!';
 console.log(c.length);
 
-console.log('Welcome to Bahamas!'.length);
+console.log('Welcome to Bahamas!'.length); //19
 
 console.log(c.toLowerCase()); //welcome to bahamas!
 console.log(c.toUpperCase()); //WELCOME TO BAHAMAS!
@@ -27,5 +27,29 @@ console.log(c.toUpperCase()); //WELCOME TO BAHAMAS!
 console.log(c.indexOf('to'));
 console.log(c.indexOf('one'));
 
-console.log(c.includes('Welcome to Bahamas!'));
+console.log(c.includes('Welcome')); 
 console.log(c.includes('welcome'));
+
+console.log(c.startsWith('Wel'));
+console.log(c.endsWith('!'));
+
+
+console.log(c.padEnd(25, '!'));
+console.log(c.padStart(23, '!'));
+
+const s = '   Hello    ';
+console.log(s.trim());
+
+
+
+
+const name = 'Богдана';
+const age = 27;
+
+// const message = `Мене звуть ${name}, мені ${age} років`;
+const message = `Дякуємо за покупку, ${name}`;
+
+
+// const message = 'Мене звуть' + ' ' + name + ' ' + 'мені' + ' ' + age + ' ' + 'років';
+console.log(message);
+
