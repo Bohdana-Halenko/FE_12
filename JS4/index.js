6bc49a21c29ff2ce7dfaaaead6b2c9e62bb2ad45
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-05-08 23:55:40 +0300

    08.05

---
+++ JS4/index.js
@@ -0,0 +1,58 @@
+// let value = 'hello';
+// console.log(Boolean(value));
+
+// &&
+
+// const number = 15;
+// const result = number > 10 && number < 20;
+// console.log(result); //true
+
+// const number = 15;
+// const result = number > 16 && number < 20;
+// console.log(result);
+
+
+// ||
+// const number = 15;
+// const result = number < 10 || number > 30;
+// console.log(result);
+
+
+// console.log(!true);
+// console.log(!0);
+
+
+
+// let cost = 0;
+// const sub = 'normal';
+
+// if (sub === 'pro') {
+//     cost = 100
+// }
+// console.log(cost);
+
+
+// let cost;
+// const sub = 'pro';
+
+// if (sub === 'pro') {
+//     cost = 100;
+// } else {
+//     cost = 0;
+// }
+
+
+let cost;
+const sub = 'premium';
+
+if (sub === 'normal') {
+    cost = 0;
+} else if (sub === 'pro') {
+    cost = 100
+} else if (sub === 'free') {
+    cost = 200;
+} else {
+    console.log("Помилка");
+}
+
+console.log(cost);
