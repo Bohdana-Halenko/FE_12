69a47814aa570dae1805696b2f71776d87f8a8fa
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-05-22 23:36:19 +0300

    22.05

---
+++ JS6/main.js
@@ -0,0 +1,15 @@
+// let number = 0;
+// while (number < 10) {
+//     console.log('number:', number);
+//     number += 1;
+// }
+
+
+
+let clientCounter = 10;
+const maxClients = 20;
+
+while (clientCounter < maxClients) {
+    console.log(clientCounter);
+    clientCounter += 1;
+}
