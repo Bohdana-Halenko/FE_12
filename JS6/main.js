e5cd8d9573c089abf19b56d0567aa29fd9e7afda
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-05-24 21:19:27 +0300

    24.05

--- JS6/main.js
+++ JS6/main.js
@@ -6,10 +6,46 @@
 
 
 
-let clientCounter = 10;
-const maxClients = 20;
+// let clientCounter = 10;
+// const maxClients = 20;
 
-while (clientCounter < maxClients) {
-    console.log(clientCounter);
-    clientCounter += 1;
+// while (clientCounter < maxClients) {
+//     console.log(clientCounter);
+//     clientCounter += 1;
+// }
+// let password = '';
+// do {
+//     password = prompt('ВВедіть пароль більше 5-ти символів', '');
+    
+// } while (password.length < 6);
+// console.log('Password:', password);
+
+
+// const max = 5;
+// for (let i = 0; i < max; i += 4){
+//     console.log(i);
+// }
+
+
+// const target = 3;
+// let sum = 0;
+
+// for (let i = 0; i <= target; i += 1){
+//     sum += i
+// }
+// console.log(sum);
+
+
+// for (let i = 0; i < 10; i += 1){
+//     if (i === 6) {
+//         break;
+//     }
+// }
+
+const sum = 10;
+for (let i = 0; i < sum; i += 1){
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log('непарні:', i);
 }
