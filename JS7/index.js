6936579bca0d4ec92176213d064168a9bfc54733
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-05-29 22:41:16 +0300

    29.05

---
+++ JS7/index.js
@@ -0,0 +1,18 @@
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// const arr = ['apple', 12, true, '', undefined];
+const fruits = ['apple', 'kiwi', 'mango'];
+console.table(fruits);
+
+// console.log(fruits[10]);
+// fruits[0] = 'orange';
+// console.table(fruits);
+
+// const arr = ['cat', 'dog', 'bird'];
+// arr[10] = 'fish';
+// console.log(arr);
+
+console.log(fruits.length);
+
+const lastIndex = fruits.length - 1;
+console.log(lastIndex);
+
