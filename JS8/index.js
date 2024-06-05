d01cf30758aeb9dafca936f13aee73e8d46cede8
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-06-06 00:33:51 +0300

    05.06

---
+++ JS8/index.js
@@ -0,0 +1,46 @@
+// const string = 'Hello, my friends!';
+// console.log(string.split(' '));
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Jana', 'Artur'];
+// console.log(names.join('-'));
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Jana', 'Artur'];
+// console.log(names.indexOf('Bobee'));
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Jana', 'Artur'];
+// console.log(names.includes('Dima'));
+
+
+// const fruits = ['apple', 'banana', 'cherry'];
+// const fruit = 'kiwi';
+
+// if (fruits.includes(fruit)) {
+//     console.log('Yes')
+// } else (console.log('No'))
+
+// const numbers = [1, 2];  //[1, 2, 5]
+// numbers.push(5);
+// console.log(numbers);
+
+// numbers.push(8);
+// console.log(numbers);
+// numbers.push('one');
+// console.log(numbers);
+
+// console.log(numbers.pop());
+// console.log(numbers);
+
+
+
+// const names = ['Alex', 'David', 'Nika', 'Bob', 'Jana', 'Artur'];
+// console.log(names.shift());
+// console.log(names);
+
+// names.unshift('Bohdan');
+// console.log(names);
+// names.unshift('Bohdana');
+
+
+const names = ['Alex', 'David', 'Nika', 'Bob', 'Jana', 'Artur'];
+// console.log(names.slice(1, 3));
+console.log(names.slice());
