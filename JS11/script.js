e40a20fb115900305ceda0cc9bbb33e118bbfa4f
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-07-02 12:11:54 +0300

    Update script.js

--- JS11/script.js
+++ JS11/script.js
@@ -1,20 +1,20 @@
-const user = {
-    name: 'Bob',
-    age: 20,
-};
-console.log(user.name);
-console.log(user['name']);
+// const user = {
+//     name: 'Bob',
+//     age: 20,
+// };
+// console.log(user.name);
+// console.log(user['name']);
 
 
-user.name = 'Alex';
-console.log(user.name);
-user['age'] = 21;
-console.log(user.age);
-console.log(user);
+// user.name = 'Alex';
+// console.log(user.name);
+// user['age'] = 21;
+// console.log(user.age);
+// console.log(user);
 
-user.address = 'Avenu';
-console.log(user);
-console.log(user.class);
+// user.address = 'Avenu';
+// console.log(user);
+// console.log(user.class);
 
 // const hotel = {
 //     name: 'Hilton',
@@ -27,19 +27,19 @@ console.log(user.class);
 // console.log(hotel);
 
 
-let name = 'Hilton';
-let stars = 5;
-const newHotel = {
-    name: name,
-    stars: stars, 
-}
+// let name = 'Hilton';
+// let stars = 5;
+// const newHotel = {
+//     name: name,
+//     stars: stars, 
+// }
 
-const hotelNew = {
-    name,
-    stars,
-    capacity: 250,
-}
-console.log(hotelNew);
+// const hotelNew = {
+//     name,
+//     stars,
+//     capacity: 250,
+// }
+// console.log(hotelNew);
 
 
 
@@ -51,12 +51,38 @@ console.log(hotelNew);
 // console.log(object);
 
 
-const key = 'person';
-const getKey = function () {
-    return 'age';
+// const key = 'person';
+// const getKey = function () {
+//     return 'age';
+// }
+// const object = {
+//     [key]: 'Nick',
+//     [getKey()]: 15,
+// }
+// console.log(object);
+
+
+
+
+
+const hotel = {
+    name: 'Hilton',
+    stars: 5,
+    get: function () {
+        console.log('Hello!');
+    },
+
+    // 6
+    getOne() {
+        console.log('Hello!');
+    }
 }
-const object = {
-    [key]: 'Nick',
-    [getKey()]: 15,
+hotel.get();
+hotel.getOne()
+
+hotel.address = function () {
+    console.log('Welcome');
 }
-console.log(object);
+hotel.address();
+
+
