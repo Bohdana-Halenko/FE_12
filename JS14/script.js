fab1e2cc616814da574ca48bf41a6aaaa282d92a
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-07-19 18:53:35 +0300

    Update script.js

--- JS14/script.js
+++ JS14/script.js
@@ -46,11 +46,57 @@
 // const doubled = numbers.map(num => num * 2);
 // console.log(doubled); //[2, 4, 6, 8, 10, 12]
 
-const users = [
-    { name: 'Bob', age: 17 },
-    { name: 'Nick', age: 27 },
-    { name: 'Alex', age: 47 },
-];
-const names = users.map(user => user.name);
-console.log(names); //['Bob', 'Nick', 'Alex']
+// const users = [
+//     { name: 'Bob', age: 17 },
+//     { name: 'Nick', age: 27 },
+//     { name: 'Alex', age: 47 },
+// ];
+// const names = users.map(user => user.name);
+// console.log(names); //['Bob', 'Nick', 'Alex']
 
+// filter
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(numbers.filter(num => num < 5)); //[1, 2, 3, 4]
+// console.log(numbers.filter(num => num > 5)); 
+// console.log(numbers.filter(num => num === 5));
+
+// const users = [
+//     {name: 'Alex', isActive: true},
+//     {name: 'Bob', isActive: false},
+//     {name: 'Nick', isActive: true},
+//     {name: 'Anna', isActive: true},
+//     {name: 'Dilan', isActive: false},
+// ];
+// const userActive = users.filter(user => user.isActive);
+// console.log(userActive);
+// const userNotActive = users.filter(user => !user.isActive);
+// console.log(userNotActive);
+
+// find
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8];
+// console.log(numbers.find(num => num > 6)); //7
+
+// const users = [
+//     {name: 'Alex', isActive: true, id: '1'},
+//     {name: 'Bob', isActive: false, id: '2'},
+//     {name: 'Nick', isActive: true, id: '3'},
+//     {name: 'Anna', isActive: true, id: '4'},
+//     {name: 'Dilan', isActive: false, id: '5'},
+// ];
+// console.log(users.find(user => user.id === '3'));
+
+
+// const numbers = val => val >= 15;
+// console.log([20, 135, 96, 10, 88, 74, 64].every(numbers)); //false
+// console.log([20, 135, 96, 100, 88, 74, 64].every(numbers)); 
+// console.log([1, 10, 8, 12, 6, 9, 5].some(numbers));
+// console.log([17, 10, 8, 12, 6, 9, 5].some(numbers));
+
+const fruits = [
+    {name: 'orange', amount: 40},
+    {name: 'kiwi', amount: 100},
+    {name: 'apple', amount: 48},
+    {name: 'banana', amount: 0},
+]
+console.log(fruits.every(fruit => fruit.amount > 0));
+console.log(fruits.some(fruit => fruit.amount > 0));
