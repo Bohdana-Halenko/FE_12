fdd792bbe75392381c17846e526472e393e3f796
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-08-01 23:55:24 +0300

    Update script.js

--- JS16/script.js
+++ JS16/script.js
@@ -43,10 +43,84 @@ employee.getWage();
 // }
 
 
-const animal = {
-    eats: true,
+// const animal = {
+//     eats: true,
+// }
+// const dog = Object.create(animal);
+// dog.barks = true;
+// const keysDog = Object.keys(dog);
+// console.log(keysDog); //['barks']
+
+
+
+// class User {
+//     #mail;
+//     constructor({ name, age, mail }) {
+//         this.name = name;
+//         this.age = age;
+//         this.#mail = mail;
+//     }
+//     getMail() {
+//         return this.#mail;
+//     }
+//     changeMail(newMail) {
+//         return this.#mail = newMail;
+//     }
+// }
+// const a = new User('Bob', '22');
+// console.log(a); //{}
+// const b = new User('Nika', '15');
+// console.log(b);
+
+// const a = new User({
+//     name: 'Bob',
+//     age: '22',
+//     mail: 'bob@gmail.com',
+// });
+// a.changeMail('newbob@gmail.com');
+// console.log(a.getMail());
+
+// console.log(a.#mail);
+
+// const b = new User({
+//     name: 'Nika',
+//     age: '15',
+// });
+// console.log(b);
+
+
+
+// get set
+class User{
+    static Roles = {
+        ADMIN: 'admin',
+        USER: 'user',
+    }
+
+    #mail;
+    #role;
+    constructor({ name, age, mail, role }) {
+        this.name = name;
+        this.age = age;
+        this.#mail = mail;
+        this.#role = role;
+    }
+    get role() {
+        return this.#role;
+    }
+    set role(newRole) {
+        this.#role = newRole;
+    }
+    
 }
-const dog = Object.create(animal);
-dog.barks = true;
-const keysDog = Object.keys(dog);
-console.log(keysDog); //['barks']
+
+const c = new User({
+    mail: 'newbob@gmail.com',
+    role: User.Roles.ADMIN,
+});
+console.log(c.Roles);
+console.log(User.Roles);
+console.log(c.role); 
+c.role = User.Roles.USER;
+onsole.log(c.role); 
+
