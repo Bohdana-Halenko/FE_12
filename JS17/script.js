093350426f4a8e52d0193bf472d92f55007c3a18
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-08-07 18:45:42 +0300

    07.08

---
+++ JS17/script.js
@@ -0,0 +1,12 @@
+// element.querySelector(selector);
+// const item = document.querySelector('.list-item');
+// const listItems = document.querySelectorAll('.list-item');
+// console.log(listItems);
+
+const listWidth =
+    document.querySelector('#menu');
+listWidth.style.textTransform = 'uppercase';
+listWidth.style.fontSize = '26px';
+listWidth.style.color = 'green';
+console.log(listWidth);
+
