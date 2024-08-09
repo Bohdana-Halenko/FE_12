f74efd3432161878ffc0208575a2dda10db6edd3
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-08-09 19:56:04 +0300

    09.08

--- JS17/script.js
+++ JS17/script.js
@@ -3,10 +3,36 @@
 // const listItems = document.querySelectorAll('.list-item');
 // console.log(listItems);
 
-const listWidth =
-    document.querySelector('#menu');
-listWidth.style.textTransform = 'uppercase';
-listWidth.style.fontSize = '26px';
-listWidth.style.color = 'green';
-console.log(listWidth);
+// const listWidth =
+//     document.querySelector('#menu');
+// listWidth.style.textTransform = 'uppercase';
+// listWidth.style.fontSize = '26px';
+// listWidth.style.color = 'green';
+// console.log(listWidth);
 
+// document.parentNode
+// document.childNodes
+// document.children
+// document.firstChild
+// document.lastChild
+// document.firstElementChild
+// document.lastElementChild
+// document.previousSibling
+// document.nextSibling
+
+
+
+// textContent
+// style.backgroundColor
+
+const button = document.querySelector('.btn');
+button.style.backgroundColor = 'orange';
+button.style.color = 'white';
+console.log(button.style);
+
+
+elem.classList.add()
+elem.classList.remove('.text');
+elem.classList.replace(old, newClass)
+elem.classList.toggle()
+elem.classList.contains('.text')
