d25d9c737755d339779fdc66714e2c5fc7e73f6d
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-08-29 11:06:01 +0300

    29/08

--- JS20/script.js
+++ JS20/script.js
@@ -24,12 +24,156 @@
 
 
 
-const element = document.getElementById('mousedown-up');
+// const element = document.getElementById('mousedown-up');
 
-element.addEventListener('mousedown', () => {
-    element.textContent = 'Кнопка миші натиснута';
-});
+// element.addEventListener('mousedown', () => {
+//     element.textContent = 'Кнопка миші натиснута';
+// });
 
-element.addEventListener('mouseup', () => {
-    element.textContent = 'Кнопка миші відпущена';
-})
+// element.addEventListener('mouseup', () => {
+//     element.textContent = 'Кнопка миші відпущена';
+// })
+
+
+
+
+
+// 9 ДЗ
+// Task 1
+// const logItems = function (array) {
+//     for (let i = 0; i < array.length; i++){
+//         console.log(`${i + 1} - ${array[i]}`);
+//     }
+// };
+
+// logItems(['Mango', 'Poly', 'Ajax', 'Lux', 'Jay', 'Kong']);
+
+// logItems([5, 10, 15, 20, 25, 30, 35, 40, 45, 50]);
+
+
+// // Task 2
+// const calculateEngravingPrice = function (message, pricePerWord) {
+//     const words = message.split(" ");
+//     const numWords = words.length;
+//     const totalPrice = numWords * pricePerWord;
+//     return totalPrice;
+// };
+
+// /*
+//  * Виклич функції для перевірки працездатності твоєї реалізації.
+//  */
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     10,
+//   ),
+// ); // 80
+
+// console.log(
+//   calculateEngravingPrice(
+//     'Proin sociis natoque et magnis parturient montes mus',
+//     20,
+//   ),
+// ); // 160
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+// ); // 240
+
+// console.log(
+//   calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+// ); // 120
+
+
+// // TAsk 3
+// const findLongestWord = function (string) {
+//     const word = string.split(" ");
+//     let longestWord = word[0];
+
+//     for (let i = 0; i < word.length; i++){
+//         if (word[i].length > longestWord.length) {
+//             longestWord = word[i];
+//         }
+//     }
+//     return longestWord;
+// };
+
+
+// console.log(findLongestWord('The quick brown fox jumped over the lazy dog')); // 'jumped'
+
+// console.log(findLongestWord('Google do a roll')); // 'Google'
+
+// console.log(findLongestWord('May the force be with you')); // 'force'
+
+
+
+// 10 HW
+// Task 1
+
+// const applyCallbackToEachElement = (arr, callback) => {
+//     const result = [];
+//     for (const element of arr) {
+//         result.push(callback(element));
+//     }
+//     return result;
+//  }
+// const arr = [1, 2, 3, 4, 5];
+// const squareCallback = (number) => number*number;
+// const result = applyCallbackToEachElement(arr, squareCallback);
+// console.log(result); // [1, 4, 9, 16, 25]
+
+
+// // Task 2
+// const calculateDiscountedPrice = (price, discount, callback) => {
+//     const discountPrice = price - (price * discount) / 100;
+//     callback(discountPrice);
+// };
+// const showDiscountedPrice = (discountPrice) => {
+//     console.log(`Discounted price: ${discountPrice}`);
+// };
+// calculateDiscountedPrice(350, 10, showDiscountedPrice); // Discounted price: 315
+
+
+// 11 HW
+// Task 1
+const bankAccount = {
+    ownerName: "Дмитро",
+    accountNumber: "123456789",
+    balance: 10000,
+
+    deposit: function () {
+        const amount = parseFloat(document.getElementById("amount").value);
+        if (isNaN(amount) || amount <= 0) {
+            document.getElementById("message").textContent = "Помилково введена сума";
+            return;
+        }
+        this.balance += amount;
+        document.getElementById("message").textContent = `Рахунок поповнено на ${amoun}грн.`;
+        updateBalance();
+    },
+
+    withdraw: function () {
+        const amount = parseFloat(document.getElementById("amount").value);
+        if (isNaN(amount) || amount <= 0) {
+            document.getElementById("message").textContent = "Помилково введена сума";
+            return;
+        }
+
+        if (amount > this.balance) {
+            document.getElementById("message").textContent = "Недостатньо коштів";
+            return;
+        }
+
+        this.balance -= amount;
+        document.getElementById("message").textContent = `Знято ${amoun}грн.`;
+        updateBalance();
+    }
+};
+
+function updateBalance() {
+    document.getElementById("ownerName").textContent = bankAccount.ownerName;
+    document.getElementById("accountNumber").textContent = bankAccount.accountNumber;
+    document.getElementById("balance").textContent = bankAccount.balance;
+}
+
+updateBalance();
