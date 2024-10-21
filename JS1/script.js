12f202911a1e9910a63b9772bc4907de0885d327
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-10-21 17:15:39 +0300

    21/10

--- JS1/script.js
+++ JS1/script.js
@@ -42,43 +42,83 @@
 // })
 
 
-const colorPalette = document.querySelector('.color-palette');
-const output = document.querySelector('.output');
+// const colorPalette = document.querySelector('.color-palette');
+// const output = document.querySelector('.output');
 
-colorPalette.addEventListener('click', selectColor);
+// colorPalette.addEventListener('click', selectColor);
 
-function selectColor(event) {
-    if (event.target.nodeName !== 'BUTTON') {
-        return;
+// function selectColor(event) {
+//     if (event.target.nodeName !== 'BUTTON') {
+//         return;
+//     }
+
+//     const selectedColor = event.target.dataset.color;
+//     output.textContent = `Selected color: ${selectedColor}`;
+//     output.style.color = selectedColor;
+// }
+
+// createPaletteItems();
+
+// function createPaletteItems() {
+//     const items = [];
+//     for (let i = 0; i < 60; i++){
+//         const color = getRandomHexColor();
+//         const item = document.createElement('button');
+//         item.type = 'button';
+//         item.dataset.color = color;
+//         item.style.backgroundColor = color;
+//         item.classList.add('item');
+//         items.push(item);
+//     }
+//     colorPalette.append(...items);
+// }
+
+// function getRandomHexColor() {
+//     const letters = '0123456789ABCDEF';
+//     let color = '#';
+
+//     for (let i = 0; i < 6; i++){
+//         color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+// }
+
+
+const taskForm = document.querySelector('#taskForm');
+const newTaskInput = document.querySelector('#newTask');
+const taskList = document.querySelector('#taskList');
+
+taskForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const taskText = newTaskInput.value.trim();
+    if (taskText !== "") {
+        addTask(taskText);
+        newTaskInput.value = "";
     }
+});
 
-    const selectedColor = event.target.dataset.color;
-    output.textContent = `Selected color: ${selectedColor}`;
-    output.style.color = selectedColor;
+function addTask(taskText) {
+    const li = document.createElement("li");
+    
+    li.innerHTML = `
+    <div>
+        <input type="checkbox" class="complateTask">
+        <span>${taskText}</span>
+    </div>
+        <button class="deleteTask">Del</button>
+    `;
+
+    taskList.appendChild(li);
 }
 
-createPaletteItems();
-
-function createPaletteItems() {
-    const items = [];
-    for (let i = 0; i < 60; i++){
-        const color = getRandomHexColor();
-        const item = document.createElement('button');
-        item.type = 'button';
-        item.dataset.color = color;
-        item.style.backgroundColor = color;
-        item.classList.add('item');
-        items.push(item);
+taskList.addEventListener('click', function (event) {
+    if (event.target.classList.contains('deleteTask')) {
+        event.target.parentElement.remove();
     }
-    colorPalette.append(...items);
-}
 
-function getRandomHexColor() {
-    const letters = '0123456789ABCDEF';
-    let color = '#';
-
-    for (let i = 0; i < 6; i++){
-        color += letters[Math.floor(Math.random() * 16)];
+    if (event.target.classList("complateTask")) {
+        const task = event.target.nextElementSibling;
+        task.style.textDecoration = event.target.checked ? 'line-through' : 'none'
     }
-    return color;
-}
+})
