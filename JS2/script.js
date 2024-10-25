e7739a4a96a76b0d11f058c458f66c10acf49cfd
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-10-25 20:35:20 +0300

    25.10

--- JS2/script.js
+++ JS2/script.js
@@ -14,16 +14,44 @@
 // }
 // console.log(total);
 
-window.addEventListener(
-    'scroll', _.throttle(() => {
-        console.log('Scroll 300ms');
-    }, 300),
+// window.addEventListener(
+//     'scroll', _.throttle(() => {
+//         console.log('Scroll 300ms');
+//     }, 3000),
+// );
+
+// const output = document.querySelector('.output');
+// let scrollCounter = 0;
+
+// document.addEventListener('scroll', () => {
+//     scrollCounter += 1;
+//     output.textContent = scrollCounter;
+// })
+
+const vanillaOutput = document.querySelector(".output.vanilla");
+const throttleOutput = document.querySelector(".output.throttle");
+const debounceOutput = document.querySelector(".output.debounce");
+const eventCounter = {
+    vanilla: 0,
+    throttle: 0,
+    debounce: 0,
+};
+
+document.addEventListener("scroll", () => {
+    eventCounter.vanilla += 1;
+    vanillaOutput.textContent = eventCounter.vanilla;
+});
+
+document.addEventListener("scroll",
+    _.throttle(() => {
+        eventCounter.throttle += 1;
+        throttleOutput.textContent = eventCounter.throttle;
+    }, 3000)
 );
 
-const output = document.querySelector('.output');
-let scrollCounter = 0;
-
-document.addEventListener('scroll', () => {
-    scrollCounter += 1;
-    output.textContent = scrollCounter;
-})
+document.addEventListener("scroll",
+    _.debounce(() => {
+        eventCounter.debounce += 1;
+        debounceOutput.textContent = eventCounter.debounce;
+    }, 300)
+)
