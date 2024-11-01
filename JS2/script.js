a2e82239b5c9ee1f765e396ca1be015a8356a49c
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-11-01 20:32:46 +0200

    01.11

--- JS2/script.js
+++ JS2/script.js
@@ -67,3 +67,22 @@
 
 
 
+function updateCoordinats(event) {
+    const graph = document.getElementById('graph');
+    const point = document.getElementById('point');
+    const coordDisplay = document.getElementById('coord');
+
+    const rect = graph.getBoundingClientRect();
+    const x = event.clientX - rect.left;
+    const y = event.clientY - rect.top;
+
+    point.setAttribute('cx', x);
+    point.setAttribute('cy', y);
+
+    coordDisplay.textContent = `X: ${x.toFixed(0)}, Y: ${y.toFixed(0)}`;
+};
+
+const throttleUpdate = _.throttle(updateCoordinats, 50);
+
+const graphEl = document.getElementById('graph');
+graphEl.addEventListener('mousemove', throttleUpdate)
