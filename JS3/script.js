594b317da19f4f3f9adb4e65c9448ad1c4846547
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-11-07 15:45:53 +0200

    08.11

--- JS3/script.js
+++ JS3/script.js
@@ -2,13 +2,3 @@
 
 // IntersectionObserver(callback, options)
 
-const options = {
-    rootMargin: '50px',
-    threshold: 0.25,
-};
-const onEntry = (entries, observer) => {
-    entries.forEach(entry => {
-        // ...
-    })
-}
-const observer = new IntersectionObserver(onEntry, options);
