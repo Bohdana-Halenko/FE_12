982ba440b70886370769061a8e2c6ad06ba26e22
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-11-09 12:02:17 +0200

    09.11

--- JS3/script.js
+++ JS3/script.js
@@ -2,3 +2,43 @@
 
 // IntersectionObserver(callback, options)
 
+// const options = {
+//     root: null,
+//     rootMargin: '0px',
+//     threshold: 0.1,
+// }
+// const observer = new IntersectionObserver((entries, observer) => {
+//     entries.forEach(entry => {
+//         if (entry.isIntersecting) {
+//             const img = entry.target;
+//             img.src = img.dataset.src;
+//             observer.unobserve();
+//         }
+//     })
+// }, options);
+
+// document.querySelectorAll('.lazy-image').forEach(img => {
+//     observer.observe(img);
+// })
+
+
+
+document.addEventListener("DOMContentLoaded", () => {
+    const fruits = document.querySelectorAll('.fruits-list li');
+
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            const img = entry.target.querySelector('img');
+            const title = entry.target.querySelector('h3');
+
+            if (entry.isIntersecting) {
+                img.src = img.getAttribute('data-high-res');
+                entry.target.classList.add("visible");
+            } else {
+                img.src = img.src.replace("-high", "-low");
+                entry.target.classList.remove("visible");
+            }
+        })
+    })
+    fruits.forEach((fruit) => observer.observe(fruit));
+})
