6e0f8bfffe21cca3ce5718656190d755e1ec7571
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2024-12-11 21:38:03 +0200

    11/12

---
+++ JS8/script.js
@@ -0,0 +1,27 @@
+// const dog = {
+//     name: "Bobyk",
+//     age: 5,
+//     isGoodBoy: true,
+//     bark() {
+//         console.log("Woof!")
+//     }
+// }
+
+// const json = JSON.stringify(dog);
+// console.log(json); //{"name":"Bobyk","age":5,"isGoodBoy":true}
+
+
+// const json1 = JSON.stringify(() => {
+//     console.log('Hello!')
+// })
+// console.log(json1); //undefined
+
+
+// console.log(JSON.parse("5")); //5
+// console.log(JSON.parse("false")); //false
+
+// const json = '{"name": "Bobyk", "age": 5, "isGoodBoy": true}';
+// const dog = JSON.parse(json);
+// console.log(dog); //
+// console.log(dog.name)
+
