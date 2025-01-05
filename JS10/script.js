da3f489c38cd05416837becac3e1ce15c3e7c23e
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-01-05 12:02:19 +0200

    05.01

--- JS10/script.js
+++ JS10/script.js
@@ -44,10 +44,71 @@
 
 
 
-const counterEl = document.getElementById('counter');
-let count = 0;
+// const counterEl = document.getElementById('counter');
+// let count = 0;
 
-setInterval(() => {
-    count++;
-    counterEl.textContent = count;
-}, 1000)
+// setInterval(() => {
+//     count++;
+//     counterEl.textContent = count;
+// }, 1000)
+
+// 4
+// document.addEventListener("DOMContentLoaded", () => {
+//     const timeInput = document.getElementById("timeInput");
+//     const output = document.getElementById("output");
+//     let timerId;
+
+//     timeInput.addEventListener('input', () => {
+//         const time = parseInt(timeInput.value);
+
+//         clearTimeout(timerId);
+//         if (isNaN(time) || time <= 0) {
+//             output.textContent = 'Введіть коректні дані';
+//             return;
+//         }
+
+//         output.textContent = `Таймер запущено! Очікуйте ${time} секунд...`;
+//         timerId = setTimeout(() => {
+//             output.textContent= 'Час вийшов! Ось ваші дані!'
+//         }, time*1000)
+//     })
+// })
+
+// 3
+function startGame() {
+    const gameArea = document.getElementById('gameArea');
+    const scoreDisplay = document.getElementById('score');
+    let score = 0;
+    const gameDuration = 10000;
+    let gameInterval;
+
+    function createTarget() {
+        const target = document.createElement('div');
+        target.className = 'target';
+
+        const maxX = gameArea.clientWidth - 50;
+        const maxY = gameArea.clientHeight - 50;
+        target.style.left = `${Math.random() * maxX}px`;
+        target.style.top = `${Math.random() * maxY}px`;
+
+        target.addEventListener('click', () => {
+            score++;
+            scoreDisplay.textContent = `Очки: ${score}`;
+            target.remove();
+        });
+
+        gameArea.appendChild(target);
+
+        setTimeout(() => target.remove(), 2000);
+    }
+
+    gameInterval = setInterval(createTarget, 1000);
+
+    setTimeout(() => {
+      clearInterval(gameInterval);
+      gameArea.innerHTML = "";
+      scoreDisplay.textContent = `Гру завершено! Ваші очки: ${score}`;
+    }, gameDuration);
+}
+
+startGame();
