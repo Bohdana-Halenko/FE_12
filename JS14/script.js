3a6beba473dfd21f2b223fa9678a3bb2014c8651
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-02-07 20:35:29 +0200

    07.02

--- JS14/script.js
+++ JS14/script.js
@@ -6,6 +6,8 @@
 // const promiseOne = makePromise('promise one', 1500);
 // const promiseTwo = makePromise('promise two', 1000);
 
+// const { determineAxis } = require("chart.js/dist/core/core.config");
+
 // Promise.all([promiseOne, promiseTwo])
 //   .then((value) => {
 //     console.log(value);
@@ -100,3 +102,76 @@
 // makePromise("Bob")
 //   .then(success => console.log(success))
 //   .catch(error => console.log(error));
+
+
+
+
+
+// Забіги
+const horses = [
+    "Хейлі",
+    "Сем",
+    "Аліса",
+    "Георгій",
+    "Луїза"
+];
+
+let raceCounter = 0;
+
+const refs = {
+  startBtn: document.querySelector(".js-start-race"),
+  winnerField: document.querySelector(".js-winner"),
+  progressField: document.querySelector(".js-progress"),
+  tableBody: document.querySelector(".js-results-table > tbody"),
+};
+
+refs.startBtn.addEventListener('click', onStart);
+
+function onStart() {
+    raceCounter += 1;
+    const promises = horses.map(run);
+    updateWinnerField('');
+    updateProgressField('Забіг вже розпочався, ставки не приймаються!');
+    determineWinner(promises);
+    waitForAll(promises);
+};
+
+function determineWinner(horsesP) {
+    Promise.race(horsesP).then(({ horse, time }) => {
+        updateWinnerField(`🎉 Переможець ${horse}, фінішував за ${time}!`);
+        updateResultsTable({horse, time, raceCounter});
+    })
+};
+
+function waitForAll(horsesP) {
+    Promise.all(promises).then(() => {
+        updateProgressField('Забіг закінчено, ставки знову приймаються!');
+    })
+};
+
+function updateWinnerField(message) {
+    refs.winnerField.textContent = message;
+};
+
+function updateProgressField(message) {
+    refs.progressField.textContent = message;
+};
+
+function updateResultsTable({ horse, time, raceCounter }) {
+    const tr = `<tr><td>${raceCounter}</td><td>${horse}</td><td>${time}</td></tr>`;
+    refs.tableBody.insertAdjacentHTML('beforeend', tr);
+};
+
+function run(horse) {
+    return new Promise(resolve => {
+        const time = getRandomTime(1500, 3000);
+
+        setTimeout(() => {
+            resolve({ horse, time });
+        }, time);
+    });
+};
+
+function getRandomTime(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
