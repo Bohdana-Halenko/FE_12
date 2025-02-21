e36ecb969ed260ae793e079478da1d530e7e8d47
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-02-21 20:33:41 +0200

    21.02

--- JS16/script.js
+++ JS16/script.js
@@ -25,13 +25,14 @@ fetchUsersBtn.addEventListener('click', () => {
 });
 
 function fetchUsers() {
-    return fetch("https://jsonplaceholder.typicode.com/users")
-        .then((response) => {
-            if (!response.ok) {
-                throw new Error(response.status)
-            }
-            return response.json();
-        });
+    return fetch(
+      "https://jsonplaceholder.typicode.com/users?_limit=3&_sort=name"
+    ).then((response) => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    });
 }
 
 function renderUsers(users) {
@@ -47,3 +48,41 @@ function renderUsers(users) {
     
     userList.insertAdjacentHTML('beforeend', markup);
 }
+
+
+// const searchParams = new URLSearchParams({
+//     _limit: 3,
+//     _sort: "name",
+// })
+// console.log(searchParams.toString()); //"_limit=5&_sort=name";
+
+// const url = `https://jsonplaceholder.typicode.com/users?${searchParams}`;
+// console.log(url); //https://jsonplaceholder.typicode.com/users?_limit=5&_sort=name
+
+
+// const headers = new Headers({
+//     "Content-Type": "application/json",
+//     "X-Custom-Header": "custom value"
+// })
+
+// fetch("https://jsonplaceholder.typicode.com/users", {
+//   headers: {
+//     "Content-Type": "application/json",
+//     "X-Custom-Header": "custom value",
+//   },
+// });
+
+
+const headers = {
+  "Accept": "application/json",
+  "Content-Type": "application/json",
+  "X-Custom-Header": "custom value 1",
+  "Authorization": "YOUR_API_TOKEN"
+};
+
+fetch("https://api.example.com/data", {
+    method: "GET",
+    headers: headers,
+}).then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error("Error:", error));
